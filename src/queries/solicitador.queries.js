f483adacc11db579c1ab98e57e110f6a6ac909b9
leoheisler <leoheisler@hotmail.com>
2024-11-04 20:28:04 -0300

    tryCatch no solicitador.criar

--- src/queries/solicitador.queries.js
+++ src/queries/solicitador.queries.js
@@ -4,8 +4,13 @@ const criar = async (cpf, nome, email, senha) => {
     const query = `INSERT INTO solicitador (cpf, nome, email, senha)
                   values ($1, $2, $3, $4) RETURNING *`;    
     const params = [cpf, nome, email, senha];
-    const dbResponse = await db.query(query,params);
-    return dbResponse.rows[0];
+    try {
+        const dbResponse = await db.query(query,params);
+        return dbResponse.rows[0];
+    } catch (error) {
+        throw new Error("Erro na Inserção")
+    }
+ 
 }
 
 const buscar = async () => {
