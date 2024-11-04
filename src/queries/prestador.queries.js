26f17f997e9947f31275a07c64f2d8a8a97d83b6
leoheisler <leoheisler@hotmail.com>
2024-11-03 21:29:01 -0300

    mudança na querie

--- src/queries/prestador.queries.js
+++ src/queries/prestador.queries.js
@@ -1,7 +1,7 @@
 import db from "../../database/connect.js";
 
 const criar = async (cpf, nome, email, senha, profissao, cep, cidade, estado) => {
-    const query = `INSERT INTO prestadores (cpf, nome, email, senha, profissao, cep, cidade, estado)
+    const query = `INSERT INTO prestador (cpf, nome, email, senha, profissao, cep, cidade, estado)
                   values ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`;    
     const params = [cpf, nome, email, senha, profissao, cep, cidade, estado];
     const dbResponse = await db.query(query,params);
@@ -9,13 +9,13 @@ const criar = async (cpf, nome, email, senha, profissao, cep, cidade, estado) =>
 }
 
 const buscar = async () => {
-    const query = `select * from prestadores;`;    
+    const query = `select * from prestador;`;    
     const dbResponse = await db.query(query);
     return dbResponse.rows;
 }
 
 const buscarPorEmail = async (email) => {
-    const query = `select * from prestadores where email = $1;`; 
+    const query = `select * from prestador where email = $1;`; 
     const params = [email];   
     const dbResponse = await db.query(query,params);
     return dbResponse.rows[0];
