9e1e6a09906ad74e36610602d5dfff24bf5e0023
Spolavore <j.vitor.spolavore@gmail.com>
2024-11-24 23:08:31 -0300

    trabalhando no agendamento

--- src/queries/agendamento.queries.js
+++ src/queries/agendamento.queries.js
@@ -1,8 +1,8 @@
 import db from "../../database/connect.js";
 
 const buscarAgendamentosPendentes = async(idPrestador) => {
-    const query = `SELECT dt_dia, dt_horario, status, descricao, titulo FROM agendamento a
-                   JOIN servico s on (a.id_servico = s.id)
+    const query = `SELECT id_agendamento,dt_dia, dt_horario, status, descricao, titulo FROM agendamento a
+                   JOIN servico_solicitado s on (a.id_servico = s.id)
                    WHERE id_prestador = $1 AND status= $2`
 
     const params = [idPrestador, 'PENDENTE'];
